1b41bfe2fb66c56d9801c7536339865d09468641
anchal4561 <anchalagrawal37@gmail.com>
2022-10-28 23:22:16 +0530

    rctd16 chakraui ass1 done

---
+++ rct_chakra_ass1_chakraui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import FlexEx from './Components/FlexEx';
+import GridBasic from './Components/GridBasic';
+import GridMain from './Components/GridMain';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <FlexEx/>
+     <GridBasic/> */}
+     <GridMain/>
+    </div>
+  );
+}
+
+export default App;
