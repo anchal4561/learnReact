30a702edab7cdb100a9ae9c0b895ed3dc78ee6db
anchal4561 <anchalagrawal37@gmail.com>
2022-10-31 15:29:20 +0530

    rct101 eva5 done

--- RCT101_evaluation/Eval5/masai-ops-68-161-bXmHpX-eceddbf0dfe28040b36c00dede7ccd7001ba6426/src/App.js
+++ RCT101_evaluation/Eval5/masai-ops-68-161-bXmHpX-eceddbf0dfe28040b36c00dede7ccd7001ba6426/src/App.js
@@ -27,47 +27,47 @@ const reducer = (state, action) => {
 };
 
 function App() {
+  
   let id=0;
-  const [state ,dispatch]=useReducer(reducer,initialState)
 
-  const [users,setUsers]=useState([])
-  const handleSubmit=(e)=>{
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-    e.preventDefault();
+  const [users, setUsers] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
     setUsers([...users,state])
     dispatch({type:"reset"})
-} 
- return (
+
+  }
+ 
+  console.log(users)
+  return (
     <div className="App">
       <div>
         <h1>User Form</h1>
         <h3>useReducer</h3>
         <div className="form-wrapper" data-testid="form-wrapper">
-          <form data-testid="form-element">
+          <form data-testid="form-element" onSubmit={handleSubmit}>
             <div className="name-wrapper" data-testid="name-wrapper">
               <label>Name</label>
               {/* keep an input tag with name attribute as "name" type as "text" and placeholder as "Name" */}
-              <input type="text" 
-               name="name"
-                placeholder="Name"
-                value={state.name}
-                onChange={(e)=>dispatch({type:"name",payload:e.target.value})}/>
-            </div>e
+              <input type="text" name="name" placeholder="Name" value={state.name} onChange={(e) => dispatch({ type:"name" , payload:e.target.value})} />
+            </div>
             <div className="gender-wrapper" data-testid="gender-wrapper">
               <label>Gender</label>
-              <select name="gender" value={state.gender} onChange={(e)=>dispatch({type:"gender",payload:e.target.value})}
-              data-testid="gender-select">
-                <option value={"male"}>Male</option>
+              <select name="gender" value={state.gender} onChange={(e) => dispatch({ type: "gender", payload: e.target.value })} data-testid="gender-select">
+                <option value={"Male"}>Male</option>
                 <option value={"Female"}>Female</option>
-                <option value={"Prefer not to say"}>Prefer not to say</option>
+                <option value={"Prefer Not to Say"}>Prefer Not to Say</option>
               </select>
             </div>
             <div className="role-wrapper" data-testid="role-wrapper">
               <label>Role</label>
-              <select name="role" value={state.role} onChange={(e)=>dispatch({type:"gender",payload:e.target.value})} data-testid="role-select">
-                <option value={"Front end"}> FrontEnd Developer</option>
-                <option value="Backend">Backend Developer</option>
-                <option value={"full stack"}>Full Stack Developer</option>
+              <select name="role" value={state.role} data-testid="role-select" onChange={(e) => dispatch({ type: "role", payload: e.target.value })}>
+                <option value={"FrontEnd Developer"}>FrontEnd Developer</option>
+                <option value={"BackEnd Developer"}>BackEnd Developer</option>
+                <option value={"FullStack Developer"}>FullStack Developer</option>
               </select>
             </div>
             <div
@@ -77,8 +77,7 @@ function App() {
               <legend>Martial Status</legend>
               <div>
                 {/* keep an input tag with type as "checkbox" and name as "maritalStatus" */}
-                <input type={"checkbox"} name="maritalStatus" checked={state.maritalStatus} onChange={(e)=>
-                dispatch({type:"maritalStatus",payload:e.target.checked})}/>
+                <input type={"checkbox"} name="maritalStatus" checked={state.maritalStatus} onChange={(e) => dispatch({ type: "maritalStatus", payload: e.target.checked })} />
                 <label>Married</label>
               </div>
             </div>
@@ -90,27 +89,27 @@ function App() {
 
         {/* map through the userdata and render UserRow component to display the data in tabular format */}
         {/* print "no users found"  in there is no user data */}
-      </div>
-    </div>
-  );
-  {users.length!=0?
-  <table>
-    <thead>
-      <tr>
-      <th>S. no</th>
+        {users.length != 0 ? 
+        <table>
+          <thead>
+            <tr>
+              <th>S. no</th>
               <th>User</th>
               <th>Gender</th>
               <th>Role</th>
               <th>Marital Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      {users.map((elem)=>(
-        <UserRow key={id+1} name={elem.name} gender={elem.gender} role={elem.role} maritalStatus={elem.maritalStatus} id={++id}/>
-      ))}
-    </tbody>
-  </table>
-  :<h2 data-testid="no-user-container">No Users</h2>}
+            </tr>
+          </thead>
+        <tbody>
+        { users.map((elem) => (
+              <UserRow key={id+1} name={elem.name} gender={elem.gender} role={elem.role} maritalStatus={elem.maritalStatus} id={++id}/>
+              ))}
+            </tbody>
+          </table>
+        : <h2 data-testid="no-user-container">no users found</h2>}
+      </div>
+    </div>
+  );
 }
 
 export default App;
