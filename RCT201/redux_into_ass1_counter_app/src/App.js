3499b9111791adf476b233b6a0102c7c681d0165
anchal4561 <anchalagrawal37@gmail.com>
2022-11-17 22:34:17 +0530

    redux_into_ass_counter&calculator done

---
+++ RCT201/redux_into_ass1_counter_app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Calculator from './Components/Calculator';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+    <Counter/>
+    <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ RCT201/redux_into_ass1_counter_app/src/redux/actionTypes.js
@@ -0,0 +1,4 @@
+export const INC="INC"
+export const DEC="DEC"
+export const MUL="MUL"
+export const DIV="DIV"
